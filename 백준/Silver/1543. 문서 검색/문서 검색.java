3d1b382a7e29fef1b5dccd997e97eab91b29058b
Yerim shin <21yrshin@naver.com>
2024-02-19 11:37:13 +0900

    [Silver V] Title: 문서 검색, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -6,20 +6,32 @@ import java.util.StringTokenizer;
 public class Main {
     static String doc, word;
     public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine(), "\n");
+//        doc = st.nextToken();
+//        st = new StringTokenizer(br.readLine(), "\n");
+//        word = st.nextToken();
+//
+//        int res = 0;
+//        while ( true ){
+//            if (!doc.contains(word)) break; // 종료 조건
+//
+//            doc = doc.replaceFirst(word, "1");
+//            res++;
+//        }
+//
+//        System.out.println(res);
+
+        // 길이 비교를 사용한 풀이
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), "\n");
-        doc = st.nextToken();
-        st = new StringTokenizer(br.readLine(), "\n");
-        word = st.nextToken();
+        String S = br.readLine();
+        String S2 = br.readLine();
 
-        int res = 0;
-        while ( true ){
-            if (!doc.contains(word)) break; // 종료 조건
+        int size = S.length();
+        int size2 = S2.length();
 
-            doc = doc.replaceFirst(word, "1");
-            res++;
-        }
-
-        System.out.println(res);
+        S = S.replace(S2, "");
+        System.out.println((size - S.length()) / size2);
     }
 }
