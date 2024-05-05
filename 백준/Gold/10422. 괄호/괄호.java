a8b7fc12b8a817300181818cfd1486f3f11c8f43
Yerim shin <21yrshin@naver.com>
2024-05-05 19:24:40 +0900

    [Gold IV] Title: 괄호, Time: 156 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Gold/10422. 괄호/괄호.java
@@ -0,0 +1,26 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        long[] dp = new long[5001];
+        dp[0] = dp[2] = 1;
+
+        // 점화식 
+        for(int i = 2; i< 2501; i++) {
+            for(int j = 0; j<i; j++) {
+                dp[i*2] += (dp[j*2]*dp[(i-1-j)*2]);
+                dp[i*2] %= 1000000007L;
+            }
+        }
+
+        for(int i=0; i < t; i++) {
+            int a = Integer.parseInt(br.readLine());
+            System.out.println(dp[a]);
+        }
+    }
+}
