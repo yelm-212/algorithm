910cf325da88d1caccb5d80f36a66135c3b4fbd5
Yerim shin <21yrshin@naver.com>
2025-03-09 13:05:52 +0900

    [Silver I] Title: 2xN 예쁜 타일링, Time: 124 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Silver/18230. 2xN 예쁜 타일링/2xN 예쁜 타일링.java
+++ 백준/Silver/18230. 2xN 예쁜 타일링/2xN 예쁜 타일링.java
@@ -7,8 +7,8 @@ import java.util.StringTokenizer;
 
 public class Main {
     private static int N, A, B, answer;
-    private static Integer[] tile_A;
-    private static Integer[] tile_B;
+    private static int[] tile_A;
+    private static int[] tile_B;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,8 +16,8 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         A = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
-        tile_A = new Integer[A];
-        tile_B = new Integer[B];
+        tile_A = new int[A];
+        tile_B = new int[B];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < A; i++) {
@@ -29,15 +29,15 @@ public class Main {
             tile_B[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 내림차순 정렬 수행
-        Arrays.sort(tile_A, Collections.reverseOrder());
-        Arrays.sort(tile_B, Collections.reverseOrder());
+        // 정렬 수행
+        Arrays.sort(tile_A);
+        Arrays.sort(tile_B);
 
 
         // 값이 홀수인 경우, A 타일을 우선적으로 사용 후 남은 타일을 배치한다.
-        int a_i = 0, b_i = 0;
+        int a_i = A - 1, b_i = B - 1;
         if (N % 2 != 0) {
-            answer += tile_A[a_i++];
+            answer += tile_A[a_i--];
             N--;
         }
 
@@ -46,19 +46,19 @@ public class Main {
 
             // 배치 가능 여부를 확인 후,
             // A 타일의 value와 B 타일의 value를 계산한다.
-            if (a_i + 1 < A){
-                tmpA = tile_A[a_i] + tile_A[a_i+1];
+            if (a_i >= 1){
+                tmpA = tile_A[a_i] + tile_A[a_i - 1];
             }
-            if (b_i < B ){
+            if (b_i >= 0){
                 tmpB = tile_B[b_i];
             }
 
             if (tmpA >= tmpB){
                 answer += tmpA;
-                a_i += 2;
+                a_i -= 2;
             }else {
                 answer += tmpB;
-                b_i++;
+                b_i--;
             }
 
             N -= 2;
