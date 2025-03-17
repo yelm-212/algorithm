4aecc352cc036cf1fd2751493b75f87054e38eb2
Yerim shin <21yrshin@naver.com>
2025-03-17 22:43:22 +0900

    [Gold V] Title: 최소비용 구하기, Time: 496 ms, Memory: 51228 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,35 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, start, end;
-    static ArrayList<Bus>[] buses;
-    static int[] dis;
-    public static void main(String[] args) throws IOException {
-        // 최소 스패닝 트리, 다익스트라 (풀이 참고함)
+    private static int N, M, start, end;
+    private static int[] distance;
+    private static ArrayList<Node>[] nodes;
+    private final static int INF = Integer.MAX_VALUE;
+
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
 
-        buses = new ArrayList[N + 1];
+        nodes = new ArrayList[N + 1];
 
-        for (int i = 0; i < N + 1; i++) {
-            buses[i] = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            nodes[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < M; i++) {
+        for(int i = 0; i < M ; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int tmps = Integer.parseInt(st.nextToken());
-            int tmpe = Integer.parseInt(st.nextToken());
-            int tmpm = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
 
-            //  방향 존재함에 주의
-            buses[tmps].add(new Bus(tmpe, tmpm));
+            nodes[s].add(new Node(e, m));
         }
-        dis = new int[N + 1];
-        Arrays.fill(dis, Integer.MAX_VALUE);
+
+        // 다익스트라 - 프림 알고리즘 초기 거리 값 INF로 설정
+        distance = new int[N + 1];
+        Arrays.fill(distance, INF);
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         start = Integer.parseInt(st.nextToken());
@@ -37,47 +37,43 @@ public class Main {
 
         Prim(start);
 
-        System.out.println(dis[end]);
-
-    }
+        System.out.println(distance[end]);    }
 
     private static void Prim(int start) {
-        Queue<Bus> q = new PriorityQueue<>(); // 우선순위 큐
-        q.add(new Bus(start, 0));
-        dis[start] = 0;
+        Queue<Node> q = new PriorityQueue<>(); // 우선순위 큐
+        q.add(new Node(start, 0));
+        distance[start] = 0;
 
         while (!q.isEmpty()){
-            Bus bus = q.poll();
-            int cur = bus.cur;
-            int curCost = bus.cost;
+            Node node = q.poll();
+            int cur = node.cur;
+            int curCost = node.cost;
 
             // 현재 정점까지 오는 비용이 다른 루트에서 방문하는 비용보다 비싸면 넘어감
             // 최적화
-            if (curCost  > dis[cur]) continue;
+            if (curCost  > distance[cur]) continue;
 
-            for (Bus nextBus : buses[cur]) {
+            for (Node next : nodes[cur]) {
                 // 다음 정점 최소 비용 > 현재 정점에서 다음 정점으로 가는 비용
                 // 이면 다음 정점 최소 비용 업데이트, 큐에 추가
-                if(dis[nextBus.cur] > nextBus.cost + curCost){
-                    dis[nextBus.cur] = nextBus.cost + curCost;
-                    q.add(new Bus(nextBus.cur, nextBus.cost + curCost));
+                if(distance[next.cur] > next.cost + curCost){
+                    distance[next.cur] = next.cost + curCost;
+                    q.add(new Node(next.cur, next.cost + curCost));
                 }
             }
         }
 
     }
+    private static class Node implements Comparable<Node> {
+        int cur, cost;
 
-    private static class Bus implements Comparable<Bus> {
-        int cur;
-        int cost;
-
-        public Bus(int cur, int cost) {
+        public Node(int cur, int cost){
             this.cur = cur;
             this.cost = cost;
         }
 
         @Override
-        public int compareTo(Bus o) {
+        public int compareTo(Node o){
             return this.cost - o.cost;
         }
     }
