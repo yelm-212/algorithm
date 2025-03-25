3d05aa6d6ab72e5716bbf077d8e677ec5edf34a7
Yerim shin <21yrshin@naver.com>
2025-03-25 23:17:31 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.08 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        double sqrt = Math.sqrt(n);
+    
+        return sqrt % 1 == 0 ? (long) Math.pow((sqrt + 1), 2) : -1;
+    }
+}
