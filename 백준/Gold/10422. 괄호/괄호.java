13550d71e4031f0fa69b7b4bb82c73ba5d90b1eb
Yerim shin <21yrshin@naver.com>
2025-04-07 01:22:16 +0900

    [Gold IV] Title: 괄호, Time: 148 ms, Memory: 14664 KB -BaekjoonHub

--- 백준/Gold/10422. 괄호/괄호.java
+++ 백준/Gold/10422. 괄호/괄호.java
@@ -1,26 +1,33 @@
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.io.BufferedReader;
-
-public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-        long[] dp = new long[5001];
-        dp[0] = dp[2] = 1;
-
-        // 점화식 
-        for(int i = 2; i< 2501; i++) {
-            for(int j = 0; j<i; j++) {
-                dp[i*2] += (dp[j*2]*dp[(i-1-j)*2]);
-                dp[i*2] %= 1000000007L;
+import java.io.*;  
+  
+public class Main {  
+    public static void main(String[] args) throws IOException{  
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  
+  
+        int t = Integer.parseInt(br.readLine());  
+        long[] dp = new long[5001];  
+        dp[0] = dp[2] = 1;  
+  
+        // 점화식  
+        for(int i = 2; i< 2501; i++) {  
+            for(int j = 0; j<i; j++) {  
+                dp[i*2] += (dp[j*2]*dp[(i-1-j)*2]);  
+                dp[i*2] %= 1_000_000_007L;  
+            }  
+        }  
+  
+        for(int i=0; i < t; i++) {  
+            int a = Integer.parseInt(br.readLine());  
+            if (a % 2 != 0) { // 홀수인 경우
+                bw.write("0\n");
+            } else {
+                bw.write(dp[a] + "\n");
             }
-        }
-
-        for(int i=0; i < t; i++) {
-            int a = Integer.parseInt(br.readLine());
-            System.out.println(dp[a]);
-        }
-    }
+        }  
+  
+        bw.flush();  
+        bw.close();  
+        br.close();  
+    }  
 }
