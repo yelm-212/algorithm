9d2fb1bdb8e8cf996ac6b7dffcc5670ba87aa64b
Yerim shin <21yrshin@naver.com>
2025-04-19 10:14:45 +0900

    [Gold IV] Title: DSLR, Time: 4556 ms, Memory: 299492 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
@@ -5,16 +6,21 @@ public class Main {
     static char[] command = {'D', 'S', 'L', 'R'};
     static boolean[] visited;
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = scan.nextInt();
+        int t = Integer.parseInt(br.readLine());
         for(int i = 0; i < t; i++) {
-            int start = scan.nextInt();
-            int target = scan.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int target = Integer.parseInt(st.nextToken());
             visited = new boolean[10000];
-            System.out.println(bfs(start, target));
+            bw.write(bfs(start, target) + "\n");
         }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
     public static String bfs(int start, int target) {
@@ -26,9 +32,10 @@ public class Main {
             Node current = q.poll();
             if(current.register == target) return current.result;
 
+            // 전체 가능한 명령어 셋을 수행하면서 큐에 넣는다.
             for(int i = 0; i < 4; i++) {
                 int change = changeRegister(current.register, command[i]);
-                if(visited[change] == false) {
+                if(!visited[change]) {
                     visited[change] = true;
                     q.offer(new Node(change, current.result + command[i]));
                 }
@@ -38,24 +45,34 @@ public class Main {
     }
 
     public static int changeRegister(int register, char command) {
-        if(command == 'D') {
-            register *= 2;
-            if(register > 9999) register %= 10000;
-        } else if(command == 'S') {
-            register -= 1;
-            if(register == -1) register = 9999;
-        } else if(command == 'L') {
-            int temp1 = (register % 1000) * 10;
-            int temp2 = register / 1000;
-            register = temp1 + temp2;
-        } else {
-            int temp1 = register / 10;
-            int temp2 = (register % 10) * 1000;
-            register = temp1 + temp2;
+
+        switch (command) {
+            case 'D':
+                register *= 2;
+                if(register > 9999) register %= 10000;
+                break;
+            case 'S':
+                register -= 1;
+                if(register == -1) register = 9999;
+                break;
+            case 'L':
+                int temp1 = (register % 1000) * 10;
+                int temp2 = register / 1000;
+                register = temp1 + temp2;
+                break;
+            case 'R':
+                int tmp1 = register / 10;
+                int tmp2 = (register % 10) * 1000;
+                register = tmp1 + tmp2;
+                break;
+            default:
+                break;
         }
+
         return register;
     }
 
+    // 현재 값의 레지스터와 결과 명령어 배열 값을 저장한다.
     public static class Node {
         int register;
         String result;
